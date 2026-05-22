2c0affe57bb89b3c474b940666fe4ab71c5066ae
João Pedro <fernandesjoaopedro622@gmail.com>
2026-05-22 20:58:03 -0300

    att aula 22/05

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,21 +1,34 @@
 package com.example.demo.Controller;
 
 import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import jakarta.websocket.server.PathParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
     UserService userService;
+    UserRepository userRepository;
 
-  public UserController(UserService userService){
-      this.userService = userService;
-  }
+    public UserController(UserService userService, UserRepository userRepository){
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
     @GetMapping ("/user/{id}")
     public User retornaUsuarioPorId(@PathVariable int id) {
        return userService.buscarPorId(id);
     }
+
+    @PostMapping("/adduser")
+    public User adicionaUsuario(@RequestBody User userTarget){
+        return userRepository.save(userTarget);
+
+    }
 }
+
